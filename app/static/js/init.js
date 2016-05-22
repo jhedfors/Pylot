960761429335c87fb1495e036ecf228c040d09bf
Jeff Hedfors <git.j@hedfors.net>
2016-05-22 15:22:37 -0700

    update

--- app/static/js/init.js
+++ app/static/js/init.js
@@ -1,3 +1,12 @@
-$(document).ready(function() {
+(function($){
+  $(function(){
+
+    
+$('.button-collapse').sideNav();
+
+  
 $('select').material_select();
-});
+}); 
+// end of document ready
+})(jQuery); 
+// end of jQuery name space
